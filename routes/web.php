9659e4c7cb291e3be21296849324661bee82d8ee
Tonymans33 <tonymansour66@yahoo.com>
2021-02-02 17:05:37 +0200

    Adding edit, update, get routes to web

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,14 @@ Route::get('/', function () {
 
 Auth::routes();
 
+
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
+// Route to go to the single item page to edit it
+Route::get('/edit/{id}', [App\Http\Controllers\HomeController::class, 'edit']);
+
+// Route to update some data in single item
+Route::post('/update/{id}', [App\Http\Controllers\HomeController::class, 'update'])->name('update');
+
 Route::post('login/{provider}/callback', 'Auth\LoginController@handleCallback');
+
